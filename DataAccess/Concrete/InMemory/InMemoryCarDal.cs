880b7b9e67b3d3d765b8ec498908341804b326ee
Sena Alzyod <l5912007@std.yildiz.edu.tr>
2021-02-28 04:14:59 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    { 
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id = 1,BrandId = 1,DailyPrice = 30000,Description = "Audi A3",ModelYear = 2015},
+                new Car{Id = 2,BrandId = 1,DailyPrice = 20000,Description = "Audi A4",ModelYear = 2017},
+                new Car{Id = 3,BrandId = 3,DailyPrice = 30000,Description = "BMW 525",ModelYear = 2005},
+            };
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(p => p.Id == Id).ToList();
+        }
+    }
+}
