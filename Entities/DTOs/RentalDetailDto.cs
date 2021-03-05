aef05e6739073ee85caaab950b48c96087fd5153
Sena Alzyod <l5912007@std.yildiz.edu.tr>
2021-03-05 16:06:32 +0300

    10.hafta

--- Entities/DTOs/RentalDetailDto.cs
+++ Entities/DTOs/RentalDetailDto.cs
@@ -10,6 +10,6 @@ namespace Entities.DTOs
         public int Id { get; set; }
         public string CarName { get; set; }
         public DateTime RentDate { get; set; }
-        public DateTime ReturnDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
