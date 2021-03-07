cbda7864cdcf1538433a5506d0df3b83a313bf14
Sena Alzyod <l5912007@std.yildiz.edu.tr>
2021-03-07 19:57:24 +0300

    12.gün

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -29,10 +31,10 @@ namespace Business.Concrete
             return new SuccessDataResult<Rental>(_rentalDal.Get(p => p.Id == id));
         }
 
+        [ValidationAspect(typeof(RentalValidator))]
         public IResult Add(Rental rental)
         {
             
-
             if (rental.ReturnDate==null)
             {
                 return new ErrorResult(Messages.CarNotAvailable);
