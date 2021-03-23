a6b435a076f6deadb7bf576829b32f20d4cc2957
Sena Alzyod <l5912007@std.yildiz.edu.tr>
2021-03-23 03:34:55 +0300

    15

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -4,6 +4,7 @@ using Business.Abstract;
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
@@ -27,32 +28,38 @@ namespace Business.Concrete
          {
              return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails()) ;
          }
+
          [SecuredOperation("car.add,admin")]
          [ValidationAspect(typeof(CarValidator))]
-         public IResult Add(Car car)
+         [CacheRemoveAspect("ICarService.Get")]
+        public IResult Add(Car car)
          {
              _carDal.Add(car);
 
              return new SuccessResult(Messages.CarAdded);
          }
 
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Delete(Car car)
         {
             _carDal.Delete(car);
             return new SuccessResult(Messages.CarDeleted);
         }
 
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Update(Car car)
         {
             _carDal.Update(car);
             return new SuccessResult(Messages.CarUpdated);
         }
 
+        [CacheAspect]
         public IDataResult<List<Car>> GetAll()
          {
              return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarsListed);
          }
 
+        [CacheAspect]
         public IDataResult<Car> GetById(int id)
         {
             return new SuccessDataResult<Car>( _carDal.Get(p => p.Id == id));
